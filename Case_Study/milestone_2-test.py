54df4ed452fa9e969854078bab785784b40a991c
Skysight2023 <124235894+Skysight2023@users.noreply.github.com>
2023-12-03 11:36:12 -0600

    Group Project
    Incorporated necessary files from group member.

--- Case_Study/milestone_2-test.py
+++ Case_Study/milestone_2-test.py
@@ -17,7 +17,7 @@ try:
     
     print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
     
-    print("\n\n Press any key to continue...")
+    
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
